257f624a32bc321038ad1951657d922835d5f79b
Subhash1009 <subhashkrishna17@gmail.com>
2023-08-08 17:31:53 +0530

    https://github.com/Subhash1009/AngularTask1

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  users = [
+    { name: 'TomCruise',movie:'Mission Impossible'},
+    { name: 'Wentworth Miller',movie:'Prison Break'},
+    { name: 'Grant Gustin',movie:'Flash'},
+    { name: 'Stephen Amell',movie:'Arrow'},
+  ];
+}
